78dbe42469612146e03b578bf2f019be8c02cffc
Görkem Gök <gorkemgok@gmail.com>
2015-01-15 14:30:54 +0200

    Added begin() method to start iteration

--- src/main/java/com/gorkemgok/data4n/core/set/IIterableDataSet.java
+++ src/main/java/com/gorkemgok/data4n/core/set/IIterableDataSet.java
@@ -5,5 +5,7 @@ import com.gorkemgok.data4n.core.row.IDataRow;
 public interface IIterableDataSet {
 	public boolean next();
 	public IDataRow getRow();
+	public void setCurrentIndex(int currentIndex);
+	public void begin();
 	public void reset();
 }
