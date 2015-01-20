33b5034f0938acb0398151d22f7c8ac4163a9fd4
Görkem Gök <gorkemgok@gmail.com>
2015-01-20 13:39:56 +0200

    Edited to process only open positions

--- src/main/java/com/gorkemgok/data4n/backtest/action/ClosePositionExpAction.java
+++ src/main/java/com/gorkemgok/data4n/backtest/action/ClosePositionExpAction.java
@@ -3,8 +3,10 @@ package com.gorkemgok.data4n.backtest.action;
 import com.gorkemgok.data4n.backtest.Position;
 import com.gorkemgok.data4n.backtest.Positions;
 import com.gorkemgok.data4n.backtest.action.result.ActionResult;
+import com.gorkemgok.data4n.backtest.action.result.ClosePositionActionResult;
 import com.gorkemgok.data4n.backtest.action.result.NoActionResult;
 import com.gorkemgok.data4n.core.set.TickDataSet;
+
 import net.objecthunter.exp4j.Expression;
 
 /**
@@ -21,13 +23,20 @@ public class ClosePositionExpAction extends ExpressionAction implements IAction{
 
     public ActionResult run(TickDataSet set) {
         super.setVariables(set);
+        ClosePositionActionResult ar = new ClosePositionActionResult();
+        boolean hasClosedPosition = false;
         for (Position position : positions.getPositions()){
-            expression.setVariable("P",position.getOpenPrice());
-            if (expression.evaluate()==1){
-                position.close(set.getRow().getClose());//TODO: hangi fiyattan alım satım yapılacagi secilebilsin
-                return new ActionResult(position);
+        	if (!position.isClosed()){
+	            expression.setVariable("P",position.getOpenPrice());
+	            double expressionResult = expression.evaluate();
+	            if (expressionResult==1d){
+	                position.close(set.getRow().getClose());//TODO: hangi fiyattan alim satim yapilacagi secilebilsin
+	                ar.getPositions().addPosition(position);
+	                hasClosedPosition = true;
+	            }
             }
         }
+        if (hasClosedPosition) return ar;
         return new NoActionResult();
     }
 }
