ca08b63f15d4cea933ff3aa26ac4c07ca05b921a
Görkem Gök <gorkemgok@gmail.com>
2015-01-28 15:50:20 +0200

    Created PositionCalculator class to perform calculation jobs

---
+++ src/main/java/com/gorkemgok/data4n/backtest/PositionCalculator.java
@@ -0,0 +1,27 @@
+package com.gorkemgok.data4n.backtest;
+
+public class PositionCalculator {
+	private Positions positions;
+	private double lastClose;
+	private double profit = 0;
+
+	public PositionCalculator(Positions positions, double lastClose) {
+		super();
+		this.positions = positions;
+		this.lastClose = lastClose;
+	}
+	
+	public void calculate(){    
+        for (Position position : positions.getPositions()){
+        	if (!position.isClosed()) position.close(lastClose);
+            profit += position.calculateProfit();
+            System.out.println(position);
+        }
+	}
+
+	public double getProfit() {
+		return profit;
+	}
+	
+	
+}
