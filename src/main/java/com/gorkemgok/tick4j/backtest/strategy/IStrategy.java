8f58a079c68e146451d7a2b67a112be193b9c263
Görkem Gök <gorkemgok@gmail.com>
2015-02-02 09:46:06 +0200

    Edited imports

--- src/main/java/com/gorkemgok/tick4j/backtest/strategy/IStrategy.java
+++ src/main/java/com/gorkemgok/tick4j/backtest/strategy/IStrategy.java
@@ -1,5 +1,8 @@
 package com.gorkemgok.tick4j.backtest.strategy;
 
+import com.gorkemgok.tick4j.backtest.Positions;
+import com.gorkemgok.tick4j.core.set.TickDataSet;
+
 /**
  * Created by gorkemgok on 19/01/15.
  */
